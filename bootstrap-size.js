46bf9933215f1b45b68d49cde7f052b112a00e1c
Jim Getty <jgett@umich.edu>
2018-02-23 10:54:21 -0500

    Add size spans dynamically if they do not exist.

--- bootstrap-size.js
+++ bootstrap-size.js
@@ -1,58 +1,68 @@
 $.fn.bootstrapSize = function (options) {
-	function Methods(target, options) {
-		var self = this;
+    function Methods(target, options) {
+        var self = this;
 
-		self.getSize = function () {
-			return target.find("[data-size]:visible").data("size");
-		};
+        self.getSize = function () {
+            return target.find("[data-size]:visible").data("size");
+        };
 
-		self.refresh = function () {
-			var sz = self.getSize();
-			var name = options.sizeNames[sz];
+        self.refresh = function () {
+            var sz = self.getSize();
+            var name = options.sizeNames[sz];
 
-			target.find(".current-size").html(name);
+            target.find(".current-size").html(name);
 
-			if (options.console)
-				console.log("current size: " + name);
-			
-			return sz;
-		}
-	}
-	
-	return this.each(function () {
-		var $this = $(this);
+            if (options.console)
+                console.log("current size: " + name);
 
-		var opt = $.extend({}, {
-			"enabled": true,
-			"console": false,
-			"sizeNames": {
-				"lg": "Large",
-				"md": "Medium",
-				"sm": "Small",
-				"xs": "Extra Small"
-			}
-		}, options, $this.data());
+            return sz;
+        }
+    }
 
-		var _currentSize = null;
+    return this.each(function () {
+        var $this = $(this);
 
-		var methods = new Methods($this, opt);
+        var opt = $.extend({}, {
+            "enabled": true,
+            "console": false,
+            "sizeNames": {
+                "lg": "Large",
+                "md": "Medium",
+                "sm": "Small",
+                "xs": "Extra Small"
+            }
+        }, options, $this.data());
 
-		$this.data("methods", methods);
+        var appendSpan = function (sz) {
+            if (!$this.find("[data-size='" + sz + "']"))
+                $this.append($("<span/>", { "class": "visible-" + sz + "-inline", "data-size": sz }));
+        };
 
-		if (opt.enabled) {
-			$(window).on("resize", function(e){
-				var sz = methods.refresh();
-				if (sz !== _currentSize){
-					var previous = {"size": _currentSize, "name": opt.sizeNames[_currentSize]};
-					var current = {"size": sz, "name": opt.sizeNames[sz]};
-					
-					_currentSize = sz;
-					
-					$this.trigger("changed.bs.size", [previous, current]);
-				}
-			});
-			
-			_currentSize = methods.refresh();
-		}
-	});
+        var _currentSize = null;
+
+        var methods = new Methods($this, opt);
+
+        $this.data("methods", methods);
+
+        if (opt.enabled) {
+            appendSpan("lg");
+            appendSpan("md");
+            appendSpan("sm");
+            appendSpan("xs");
+
+            $(window).on("resize", function (e) {
+                var sz = methods.refresh();
+                if (sz !== _currentSize) {
+                    var previous = { "size": _currentSize, "name": opt.sizeNames[_currentSize] };
+                    var current = { "size": sz, "name": opt.sizeNames[sz] };
+
+                    _currentSize = sz;
+
+                    $this.trigger("changed.bs.size", [previous, current]);
+                }
+            });
+
+            _currentSize = methods.refresh();
+        }
+    });
 };
