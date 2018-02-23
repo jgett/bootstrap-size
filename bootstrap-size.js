58f8a66b23b0ad2be113a270b53e4f6e15a6ad26
Jim Getty <jgett@umich.edu>
2018-02-23 10:36:32 -0500

    First commit.

---
+++ bootstrap-size.js
@@ -0,0 +1,58 @@
+$.fn.bootstrapSize = function (options) {
+	function Methods(target, options) {
+		var self = this;
+
+		self.getSize = function () {
+			return target.find("[data-size]:visible").data("size");
+		};
+
+		self.refresh = function () {
+			var sz = self.getSize();
+			var name = options.sizeNames[sz];
+
+			target.find(".current-size").html(name);
+
+			if (options.console)
+				console.log("current size: " + name);
+			
+			return sz;
+		}
+	}
+	
+	return this.each(function () {
+		var $this = $(this);
+
+		var opt = $.extend({}, {
+			"enabled": true,
+			"console": false,
+			"sizeNames": {
+				"lg": "Large",
+				"md": "Medium",
+				"sm": "Small",
+				"xs": "Extra Small"
+			}
+		}, options, $this.data());
+
+		var _currentSize = null;
+
+		var methods = new Methods($this, opt);
+
+		$this.data("methods", methods);
+
+		if (opt.enabled) {
+			$(window).on("resize", function(e){
+				var sz = methods.refresh();
+				if (sz !== _currentSize){
+					var previous = {"size": _currentSize, "name": opt.sizeNames[_currentSize]};
+					var current = {"size": sz, "name": opt.sizeNames[sz]};
+					
+					_currentSize = sz;
+					
+					$this.trigger("changed.bs.size", [previous, current]);
+				}
+			});
+			
+			_currentSize = methods.refresh();
+		}
+	});
+};
